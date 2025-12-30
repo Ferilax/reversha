473b2538e5a4a2149c1257bf495573f57adff608
Ferilax <muhammadfx@mail.ru>
2025-12-30 13:29:00 +0300

    wheel fix

--- js/wheel.js
+++ js/wheel.js
@@ -7,16 +7,13 @@ import { animate } from "https://cdn.jsdelivr.net/npm/motion@12.23.26/+esm"
 	let rotates = 0
 
 	wheelSection.addEventListener("click", () => {
+
+		rotates === 2 ? rotates = 0 : rotates++
+
 		animate(
 			wheel,
 			{ rotate: -90 * rotates },
 			{ type: "spring", stiffness: 200, damping: 30, mass: 2 }
 		)
-
-		if (rotates === 2) {
-			rotates = 0
-		} else {
-			rotates++
-		}
 	})
 }())
